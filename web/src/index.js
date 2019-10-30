d2471ff95bf9653f7a2ccb22279543b96fe3f2b4
aygao <annyygao@gmail.com>
2019-10-30 10:13:16 -0400

    add fonts from google fonts

--- web/src/index.js
+++ web/src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import WebFont from 'webfontloader';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+WebFont.load({
+    google: {
+      families: ['Lobster:300,400,700', 
+        'Quicksand:300,400,700', 
+        'Courgette:300,400,700',
+        'sans-serif']
+    }
+  });
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
